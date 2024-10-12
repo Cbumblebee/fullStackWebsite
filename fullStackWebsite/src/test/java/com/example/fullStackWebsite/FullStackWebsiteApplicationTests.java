9db8aa00f90889f14034fc732e9bc8a1a4782a10
Maja <maja.schachreiter@gmail.com>
2024-10-12 18:53:24 +0200

    first initialization

---
+++ fullStackWebsite/src/test/java/com/example/fullStackWebsite/FullStackWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullStackWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
